6295b2b38d00003560cb134fb0b86507c1e3910d
felipebool <bolzin@gmail.com>
2016-07-10 10:34:49 -0300

    renaming

--- user/user.py
+++ user/user.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+from user_dtp import UserDTP
+from user_pi import UserPI
 
 class User():
 	'''
 		File Transfer Protocol User Interface
 	'''
-	pass
+	prompt_msg = 'ftpy> '
+
+	def __init__(self):
+		pass
+
+if __name__ == '__main__':
+	interface = UserInterface()
+
+	while True:
+		command = input(UserInterface.prompt_msg)
+		print(command)
+
+		if command == 'quit':
+			break
+
+	# end connection
